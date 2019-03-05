67ef5c6aac14b857a81b750ea56509843732db71
Wonkeun <Wonkeun@DESKTOP-DTM0O5V>
2019-03-05 14:50:14 +0900

    index에서 세션주는거 추가

--- FinalProject2/src/main/java/com/spring/test/test.java
+++ FinalProject2/src/main/java/com/spring/test/test.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("userNo")
 public class test {
 
 		private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -19,9 +21,11 @@ public class test {
 		/**
 		 * Simply selects the home view to render by returning its name.
 		 */
+		
+		
 		@RequestMapping(value = "/gaga", method = RequestMethod.GET)
-		public String gaga() {
-			
+		public String gaga(Model model) {
+			model.addAttribute("userNo", 1);
 
 			return "main";
 		}
