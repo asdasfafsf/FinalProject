f79c4a41927e8a3307682cf75cb31b5e8743609c
User <User@DESKTOP-3GRM568>
2019-03-28 14:14:41 +0900

    오류 막음

--- FinalProject2/src/main/java/com/spring/test/common/util/Scheduler.java
+++ FinalProject2/src/main/java/com/spring/test/common/util/Scheduler.java
@@ -5,21 +5,22 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.spring.test.reward.model.dao.RewardDao;
+import com.spring.test.reward.model.service.RewardService;
 
 @Component
 public class Scheduler {
 
-	@Autowired
-	RewardDao dao;
+	/*@Autowired
+	RewardDao dao;*/
 	
 	//매 0시마다 실행됨
 	@Scheduled(cron ="0 0 0 * * *")
 	public void cronTest1() {
-		dao.updateStateOpenSchedule();
+	/*	dao.updateStateOpenSchedule();
 		dao.updateRewardOpen();
 		dao.updateOpenScheduleRewardOpen();
 		dao.updateRewardSuccess();
-		dao.updateRewardFail();
+	    dao.updateRewardFail();*/
 	}
 	
 	
